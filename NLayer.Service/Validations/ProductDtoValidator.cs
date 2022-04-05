ef80f9fc05db37d1fbf0328f8e2bb5147b07166b
fatihseftali@gmail.com <fatihseftali@gmail.com>
2022-04-05 22:26:00 +0300

    Asp.net core mvc app

--- NLayer.Service/Validations/ProductDtoValidator.cs
+++ NLayer.Service/Validations/ProductDtoValidator.cs
@@ -15,8 +15,7 @@ namespace NLayer.Service.Validations
             RuleFor(x => x.Name).NotNull().WithMessage("{PropertyName} is Required").NotEmpty().WithMessage("{PropertyName} is Required");
 
             RuleFor(x => x.Price).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater 0");
-            RuleFor(x => x.Stock).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater 0");
-            RuleFor(x => x.CategoryId).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater 0");
+            RuleFor(x => x.Stock).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater 0");           
         }    
     }
 }
